ae8524eafc570936ff35c17cf128846087539248
vinodh1988 <k.c.vinodh@hotmail.com>
2018-09-04 19:40:36 +0530

    Third Commit

--- HibernateDemo/src/com/runner/Runner.java
+++ HibernateDemo/src/com/runner/Runner.java
@@ -1,10 +1,12 @@
 package com.runner;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
-import com.model.Jobs;
+import com.model.Jobs1;
 import com.model.SessionProvider;
 
 public class Runner {
@@ -15,15 +17,25 @@ public class Runner {
 		SessionFactory s=SessionProvider.getSessionFactory();
 		Session s1=s.openSession();
 		
-		Transaction t=s1.beginTransaction();
+		/*Transaction t=s1.beginTransaction();
 		
-		Jobs j=new Jobs("PM","Project Manager",50000,70000);
-		
-		s1.save(j);
-		
+		Jobs j=new Jobs("SE","Software Engineer",30000,90000);
+		s1.update(j);
+	    j.setMax_salary(80000);
+	    s1.update(j);
+	    j.setJob_title("System Engineer");
+	    s1.update(j);
+		s1.delete(j);
 		t.commit();
 		
-		System.out.println("Row inserted");
+		System.out.println("Row Updated");*/
+		
+		Query q=s1.createQuery("from Jobs1 where min_salary>7000 order by job_title");
+		
+		List<Jobs1> l=q.list();
+		
+		for(Jobs1 x:l)
+			System.out.println(x.getJob_title());
 		
 	}
 
